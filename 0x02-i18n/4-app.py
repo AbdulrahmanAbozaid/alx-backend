43d868772444adaba09d0a5524c6dcac28ee86f5
AbdulrahmanAbozaid <diver72003@gmail.com>
2025-04-27 22:26:47 +0300

    Edit file

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,11 +21,15 @@ babel = Babel(app)
 def get_locale():
     """Get the best locale"""
     return request.args.get("locale") or request.accept_languages.best_match(
-        ["en", "ar"]
+        ["en", "fr"]
     )
 
 
 @app.route("/")
 def hello():
     """Main entry"""
-    return render_template("0-index.html")
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
