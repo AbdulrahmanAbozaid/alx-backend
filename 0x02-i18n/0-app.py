2b473785ef59f4685cd2fad272a188a107bcce09
AbdulrahmanAbozaid <diver72003@gmail.com>
2025-04-27 20:21:14 +0300

    Edit file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """Minimal app"""
-from flask import Flask, request, render_template
-from flask_babel import Babel
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def hello():
     """Main entry"""
-    return render_template('0-index.html')
+    return render_template("0-index.html")
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Minimal app"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Store the app configs"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello():
+    """Main entry"""
+    return render_template("0-index.html")
